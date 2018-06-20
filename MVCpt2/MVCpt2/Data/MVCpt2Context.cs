776861ba2e79b719ac27ba8e8f3f1a700bf15637
Max <gusstorm132@gmail.com>
2018-06-20 11:31:08 -0700

    on step 5

---
+++ MVCpt2/MVCpt2/Data/MVCpt2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCpt2.Models
+{
+    public class MVCpt2Context : DbContext
+    {
+        public MVCpt2Context (DbContextOptions<MVCpt2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCpt2.Models.Movie> Movie { get; set; }
+    }
+}
